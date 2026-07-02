2d744b0d592311a0cce7c19c891733a0947ddc7b
Lavay Garg <garglavay@gmail.com>
2026-07-02 21:09:02 +0530

    replace uuid-based session management with JWT authentication and remove unnecessary dependencies

--- controllers/user.js
+++ controllers/user.js
@@ -1,5 +1,5 @@
 const User = require("../models/user");
-const { v4: uuidv4 } = require("uuid");
+
 const {setUser}=require('../services/auth');
 const {getUser}=require('../services/auth');
 async function handleUserSignup(req, res) {
@@ -9,9 +9,8 @@ async function handleUserSignup(req, res) {
         email,
         password
     });
-    const sessionId = uuidv4();
-    setUser(sessionId, user);
-    res.cookie("uid", sessionId);
+    const token = setUser(user);
+    res.cookie("uid", token);
     return res.redirect('/');
 }
 async function handleUserLogin(req,res){
@@ -27,9 +26,8 @@ async function handleUserLogin(req,res){
             error:"Invalid Password"
         });
     }
-    const sessionId=uuidv4();
-    setUser(sessionId,user);
-    res.cookie("uid",sessionId);
+    const token = setUser(user);
+    res.cookie("uid", token);
     return res.redirect('/');
 }
 module.exports={
