b326a28c09e10b8edd4fd3c6200e1850cce28cc8
Lavay Garg <garglavay@gmail.com>
2026-07-02 21:52:11 +0530

    implement Bearer token support in authentication middleware

--- middlewares/auth.js
+++ middlewares/auth.js
@@ -1,14 +1,22 @@
 const {getUser}=require('../services/auth');
 async function restrictToLoggedinUserOnly(req,res,next){
-    const userUid=req.cookies?.uid;
-    const user = getUser(userUid);
-    if(!user)return res.redirect('/login');
+    let token = req.cookies?.uid;
+    const authHeader = req.headers["authorization"] || req.headers["Authorization"];
+    if (authHeader && authHeader.startsWith("Bearer ")) {
+        token = authHeader.split(" ")[1];
+    }
+    const user = getUser(token);
+    if(!user) return res.redirect('/login');
     req.user=user;
     next();
 }
 async function checkAuth(req,res,next){
-    const userUid=req.cookies?.uid;
-    const user = getUser(userUid);
+    let token = req.cookies?.uid;
+    const authHeader = req.headers["authorization"] || req.headers["Authorization"];
+    if (authHeader && authHeader.startsWith("Bearer ")) {
+        token = authHeader.split(" ")[1];
+    }
+    const user = getUser(token);
     req.user=user;
     next();
 }
